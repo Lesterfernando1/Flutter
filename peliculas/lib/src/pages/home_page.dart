db03aafbc958abd36e878e7163975cf400783102
Lester <lesterfernaando@gmail.com>
2020-07-22 19:07:31 -0400

    Movies horizontales agregeados

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage  extends StatelessWidget {
 
@@ -65,16 +66,26 @@ class HomePage  extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Populares', style: Theme.of(context).textTheme.subhead,),
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            // ignore: deprecated_member_use
+            child: Text('Populares', style: Theme.of(context).textTheme.subhead,)
+          ),
+          SizedBox(height: 10.0,),
 
           FutureBuilder(
             
             future: peliculasProvider.getPolulares(),
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-
-              snapshot.data?.forEach((p)=> print(p.title));
-              return Container();
+              
+              if(snapshot.hasData){
+                return MovieHorizontal(peliculas: snapshot.data);
+              } else{
+                return Center(child: CircularProgressIndicator()); 
+              }
+      
             },
           ),
 
--- peliculas/lib/src/providers/peliculas_provider.dart
+++ peliculas/lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,9 @@
+
+
 import 'package:http/http.dart' as http;
 
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
 
@@ -9,6 +12,22 @@ class PeliculasProvider{
   String _apikey   ='c6acdb8db4e4a5f941071c6c68a64be2';
   String _url      ='api.themoviedb.org';
   String _lenguage ='es-ES';
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares =new List();
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast(); //con el broad se puese usar de muchos lugares.
+
+
+  Function(List<Pelicula>) get polularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+
+  void disposeStreams(){
+    _popularesStreamController?.close(); //si tiene el metodo close, se llama
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
 
@@ -36,12 +55,24 @@ class PeliculasProvider{
 
   Future<List<Pelicula>>  getPolulares() async {
 
+    _popularesPage++;
+
      final url =Uri.https(_url, '3/movie/popular',{
       'api_key'  : _apikey,
       'language' : _lenguage,
+      'page'     : _popularesPage.toString()
     });
+    
+    final resp =  await _procesarRespuesta(url);
 
-    return await _procesarRespuesta(url);
+    _populares.addAll(resp);
+
+    polularesSink(_populares);
+
+    return resp;
+
+
+    
   }
 
 
